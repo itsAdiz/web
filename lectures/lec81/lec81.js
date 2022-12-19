f3a76085b328928d8df67ccee7a506f6b2d9da75
Rana Adnan <adnanz@pm.me>
2022-12-19 15:42:14 +0500

    lec 81 added

---
+++ lectures/lec81/lec81.js
@@ -0,0 +1,14 @@
+// while loop code 
+let i = 0;
+while(i<10){
+    document.getElementById("whileLoop").innerHTML += i + "<br>";
+    i++;
+}
+
+
+// do while loop code
+let j = 0;
+do{
+    document.getElementById("doWhileLoop").innerHTML += j + "<br>";
+    j++;
+}while(j<10);
